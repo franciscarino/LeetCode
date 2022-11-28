fc071e73dce995efc4892cc7bebe3f45327b848d
Francis Carino <fgcarino@gmail.com>
2022-11-27 22:26:33 -0800

    complete 20 valid parentheses

--- 20-valid-parentheses.js
+++ 20-valid-parentheses.js
@@ -1,5 +1,39 @@
 'use strict';
 
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * 
+ * Input: s = "()"
+ * Output: true
+ * 
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * Input: s = "(]"
+ * Output: false
+ */
 function isValid(s) {
+  const openSymbols = [];
 
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+      openSymbols.push(s[i]);
+    } else if (s[i] === ')' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '(') {
+      openSymbols.pop();
+    } else if (s[i] === ']' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '[') {
+      openSymbols.pop();
+    } else if (s[i] === '}' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '{') {
+      openSymbols.pop();
+    } else {
+      return false;
+    }
+  }
+  return openSymbols.length === 0;
 }
