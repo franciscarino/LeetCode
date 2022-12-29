60178746194e95af20465023a77a0c21bf5acad5
Francis Carino <fgcarino@gmail.com>
2022-12-28 18:01:33 -0800

    complete 412

--- 412-fizz-buzz.js
+++ 412-fizz-buzz.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function fizzBuzz(n) {
+  const output = [];
 
-} 
+  for (let i = 1; i < n + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      output.push('Fizz');
+    } else if (i % 5 === 0) {
+      output.push('Buzz');
+    } else {
+      output.push(String(i));
+    }
+  }
+  return output;
+}
