5cb108c5195bcb3c26f3125df81e9d143759a0ef
Francis Carino <fgcarino@gmail.com>
2022-12-29 22:33:34 -0800

    complete 876

--- 876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list.js
@@ -1,2 +1,12 @@
 'use strict';
 
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
