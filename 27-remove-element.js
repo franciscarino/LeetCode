59ec6dfca748031f04af7102d21b48c7423219a4
Francis Carino <fgcarino@gmail.com>
2022-12-30 16:30:04 -0800

    complete 27

--- 27-remove-element.js
+++ 27-remove-element.js
@@ -1,2 +1,13 @@
 'use strict';
 
+function removeElement(nums, val) {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return nums.length;
+}
