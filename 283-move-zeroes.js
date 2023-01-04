63aabbedcd027cf049fcab2629086ec5bd6d5295
Francis Carino <fgcarino@gmail.com>
2023-01-04 15:42:38 -0800

    complete 283

--- 283-move-zeroes.js
+++ 283-move-zeroes.js
@@ -4,5 +4,16 @@
 //Output: [1,3,12,0,0]
 
 function moveZeroes(nums) {
-
+  let zeroCount = 0;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      zeroCount++;
+    }
+  }
+  while (zeroCount) {
+    nums.push(0);
+    zeroCount--;
+  }
+  return nums;
 }
