9cdee4b9a99429cea9089fcea689aa21013fc5cf
Francis Carino <fgcarino@gmail.com>
2023-01-07 22:01:18 -0800

    complete 94

--- 94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function inorderTraversal(root) {
+  let res = [];
+  let stack = [];
+  let current = root;
+  while (current !== null || stack.length !== 0) {
+    while (current !== null) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    res.push(current.val);
+    current = current.right;
+  }
+  return res;
+}
