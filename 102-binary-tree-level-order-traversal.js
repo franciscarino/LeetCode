2bdf97e7835734c6178825bdbe91c984d32e9185
Francis Carino <fgcarino@gmail.com>
2023-01-09 22:24:03 -0800

    complete 102

--- 102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function levelOrder(root) {
-
+  if (!root) return [];
+  const result = [];
+  let queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < size; i++) {
+      const currentNode = queue.shift();
+      currentLevel.push(currentNode.val);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    result.push(currentLevel);
+  }
+  return result;
 }
