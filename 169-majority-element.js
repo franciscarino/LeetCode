0f2214cfebaf9b950205376e05935ed82eab42f5
Francis Carino <fgcarino@gmail.com>
2023-01-10 16:52:22 -0800

    complete 169

--- 169-majority-element.js
+++ 169-majority-element.js
@@ -1,5 +1,30 @@
 'use strict';
 
 function majorityElement(nums) {
+  const numCounts = freqCounter(nums);
+  console.log("numCounts:", numCounts);
 
+  let keyList = Object.values(numCounts);
+  console.log("keyList", keyList);
+  let maxNum = Math.max(...keyList);
+  console.log("maxNum", maxNum);
+
+  for (let num in numCounts) {
+    console.log('num: ', num);
+    if (numCounts[num] === maxNum) {
+      return num;
+    }
+  }
+}
+
+function freqCounter(nums) {
+  let numObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] in numObj)) {
+      numObj[nums[i]] = 1;
+    } else {
+      numObj[nums[i]]++;
+    }
+  }
+  return numObj;
 }
