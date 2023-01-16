1e0ef325f462165ecf3e9dad44f177d3609e566e
Francis Carino <fgcarino@gmail.com>
2023-01-15 20:21:01 -0800

    complete 1984

--- 1984 -minimum-difference-between-highest-and-lowest-of-K-scores.js
+++ 1984 -minimum-difference-between-highest-and-lowest-of-K-scores.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function minimumDifference(nums, k) {
+  nums.sort((a, b) => a - b);
+  let min = Number.MAX_VALUE;
+  for (let i = 0; i <= nums.length - k; i++) {
+    min = Math.min(min, nums[i + k - 1] - nums[i]);
+  }
+  return min;
+};
