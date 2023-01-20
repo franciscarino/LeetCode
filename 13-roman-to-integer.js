d7e27ec05665a9de310d9d3e27cb28be6ab599db
Francis Carino <fgcarino@gmail.com>
2023-01-19 21:13:13 -0800

    complete 13

--- 13-roman-to-integer.js
+++ 13-roman-to-integer.js
@@ -1,5 +1,32 @@
 'use strict';
 
-function romanToInt(s) {
+/**
+ * Input: s = "III" 
+ * Output: 3 
+ *
+ * Input: s = "LVIII"
+ * Output: 58
+ *
+ */
 
+function romanToInt(s) {
+  const roman = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+
+  let output = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (roman[s[i]] < roman[s[i + 1]]) {
+      output -= roman[s[i]];
+    } else {
+      result += roman[s[i]];
+    }
+  }
+  return result;
 }
