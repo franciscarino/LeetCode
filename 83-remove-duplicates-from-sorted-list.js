63b9b4eff08e5d8b3026542c5a3b0b9b17fbe859
Francis Carino <fgcarino@gmail.com>
2023-01-21 21:55:31 -0800

    complete 83

--- 83-remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function deleteDuplicates(head) {
-
+  let current = head;
+  while (current !== null && current.next !== null) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
 }
