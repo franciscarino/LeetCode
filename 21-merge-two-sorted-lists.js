534861dbfb6c7b948f12068183c4ac9e1d67313c
Francis Carino <fgcarino@gmail.com>
2023-01-22 21:15:29 -0800

    complete 21

--- 21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists.js
@@ -5,7 +5,23 @@
  * Return the head of the merged linked list.
  */
 
-//TODO:
 function mergeTwoLists(list1, list2) {
+  if (!list1) return list2;
+  if (!list2) return list1;
 
+  let newNode = new ListNode();
+  let current = newNode;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  current.next = list1 || list2;
+  return newNode.next;
 }
