3baee440daa109cabf10248c422c57f229daa87f
Francis Carino <fgcarino@gmail.com>
2023-01-24 21:35:04 -0800

    complete 66

--- 66-plus-one.js
+++ 66-plus-one.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function incrementInteger(digits) {
-
+  for (let i = digits.length - 1: i>= 0; i--) {
+    if (digits[i] = 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
 }
