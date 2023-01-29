e3e19277f982ca90cac1142ecee22557e3762fe1
Francis Carino <fgcarino@gmail.com>
2023-01-28 21:35:09 -0800

    complete 520

--- 520-delete-capital.js
+++ 520-delete-capital.js
@@ -1,5 +1,13 @@
 "use strict";
 
 function detectCapitalUse(word) {
-
+  if (word === word.toUpperCase()) {
+    return true;
+  } else if (word === word.toLowerCase()) {
+    return true;
+  } else if (word[0] === word[0].toUpperCase()) {
+    return true;
+  } else {
+    return false;
+  }
 }
